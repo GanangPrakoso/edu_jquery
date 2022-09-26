41ce9917172baa942fa9aa98590dca9934a1d703
GanangPrakoso <ganangmanggolo@gmail.com>
2022-09-26 22:27:34 +0700

    finish state

---
+++ main.js
@@ -0,0 +1,90 @@
+const baseUrl = "http://localhost:3000";
+
+function deleteMovie(id) {
+  console.log(id);
+}
+
+function fetchMovie() {
+  $.ajax({
+    url: baseUrl + "/movies",
+    method: "get",
+    headers: {
+      access_token: localStorage.access_token,
+    },
+  })
+    .done((data) => {
+      // $("#table-body").empty();
+      let HTMLliteral = "";
+
+      data.forEach((el) => {
+        let temp = `
+                      <tr>
+                  <td>
+                    <img
+                      src="${el.image_url}"
+                      style="width: 200px; height: 300px; object-fit: cover"
+                    />
+                  </td>
+                  <td>${el.name}</td>
+                  <td onclick="deleteMovie(${el.id})">${el.description}</td>
+                </tr>
+                      `;
+
+        HTMLliteral = HTMLliteral + temp;
+
+        //       $("#table-body").append(temp);
+      });
+
+      $("#table-body").html(HTMLliteral);
+    })
+    .fail((err) => {
+      console.log(err);
+    });
+}
+
+$(document).ready(() => {
+  // navigation guard
+  console.log("kontol root");
+
+  if (localStorage.access_token) {
+    $("#home-page").show();
+    $("#login-page").hide();
+    fetchMovie();
+  } else {
+    $("#home-page").hide();
+    $("#login-page").show();
+  }
+
+  $("#form-login").on("submit", (event) => {
+    event.preventDefault();
+
+    let email = $("#input-login-email").val();
+    let password = $("#input-login-password").val();
+
+    $.ajax({
+      url: baseUrl + "/users/login",
+      method: "post",
+      data: {
+        email,
+        password,
+      },
+    })
+      .done((data) => {
+        console.log(data); // {access_token: 'asldfjlsd'}
+        localStorage.setItem("access_token", data.access_token);
+        $("#home-page").show();
+        $("#login-page").hide();
+
+        fetchMovie();
+      })
+      .fail((err) => {
+        console.log(err);
+      });
+  });
+
+  $("#button-logout").on("click", () => {
+    localStorage.clear();
+    $("#home-page").hide();
+    $("#login-page").show();
+  });
+});
